bf19a7476cbcf333e10b3114dee8b735ecfe8ff4
asadkhan777 <asadkhan1776@gmail.com>
2022-05-14 05:24:35 +0530

    Add necessary application class to help integrate Suprsend SDK

---
+++ example/android/app/src/main/kotlin/com/suprsend/suprsend_flutter_sdk_example/SuprsendFlutterPluginTestApplication.kt
@@ -0,0 +1,21 @@
+package com.suprsend.suprsend_flutter_sdk_example
+
+import android.app.Application
+import app.suprsend.SSApi
+import com.suprsend.suprsend_flutter_sdk_example.BuildConfig.*
+
+class SuprsendFlutterPluginTestApplication: Application() {
+
+  companion object {
+    const val TAG = "ClientSuprsend$VERSION_NAME"
+    lateinit var application: SuprsendFlutterPluginTestApplication
+  }
+
+  override fun onCreate() {
+    application = this
+    SSApi.init(this, SS_API_KEY, SS_API_SECRET, SS_API_BASE_URL)
+    SSApi.initXiaomi(this, XIAOMI_APP_ID, XIAOMI_API_KEY)
+    super.onCreate()
+  }
+
+}
